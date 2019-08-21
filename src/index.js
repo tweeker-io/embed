5537c1f1140ddcfce582ad9cca306de551a06c19
Ryan MacInnes <ryan.macinnes@gmail.com>
2019-08-21 14:47:43 -0700

    Rempving babel-dotenv.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import { API_ROOT, SCRIPT_VERSION, NODE_ENV, BUSINESS_ID } from 'babel-dotenv';
-
 window.TweekerData = {};
 
 const run = () => {
@@ -20,16 +18,16 @@ const fetchVariants = () => {
 }
 
 const parseBusinessId = () => {
-  if (NODE_ENV === 'development') { return BUSINESS_ID; }
-  const script = window.currentScript || document.querySelector(`script[src*="${SCRIPT_VERSION}.js"]`)
+  if (process.env.NODE_ENV === 'development') { return process.env.BUSINESS_ID; }
+  const script = window.currentScript || document.querySelector(`script[src*="${process.env.SCRIPT_VERSION}.js"]`)
   const src = script.src
-  const existing = ['http:', 'https:', '', 'localhost:9000', 'cdn.tweeker.io', 'embed', `${SCRIPT_VERSION}.js`, 'latest.js']
+  const existing = ['http:', 'https:', '', 'localhost:9000', 'cdn.tweeker.io', 'embed', `${process.env.SCRIPT_VERSION}.js`, 'latest.js']
   let array = src.split('/')
   const remaining = array.filter(element => !existing.includes(element))
   return remaining[0]
 }
 
-const variantsUrl = `${API_ROOT}/v1/tests/embed?url=${encodeURIComponent(window.location.href)}&business_id=${parseBusinessId()}`
+const variantsUrl = `${process.env.API_ROOT}/v1/tests/embed?url=${encodeURIComponent(window.location.href)}&business_id=${parseBusinessId()}`
 
 const handleVariants = (data) => {
   if (data.variants.length < 1) { return; }
@@ -60,7 +58,7 @@ const bindGoals = (goals) => {
 }
 
 const handleGoal = (event) => {
-  const url = API_ROOT + '/v1/successes';
+  const url = process.env.API_ROOT + '/v1/successes';
   const target = event.target;
 
   return fetch(url, {
