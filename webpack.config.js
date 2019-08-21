62a974cdcc72ae6b1e1191f1573f4bdf8fcc17ac
Ryan MacInnes <ryan.macinnes@gmail.com>
2019-08-21 16:00:59 -0700

    Roll my own dotenv setup.

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,41 @@
-var path = require('path');
+const path = require('path');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
+const fs = require('fs');
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
+module.exports = (env) => {
+  const currentPath = path.join(__dirname);
+  const basePath = currentPath + '/.env';
+  const envPath = basePath + '.' + env.ENVIRONMENT;
+  const finalPath = fs.existsSync(envPath) ? envPath : basePath;
+  const fileEnv = dotenv.config({ path: finalPath }).parsed;
+  const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
+    return prev;
+  }, {});
+
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
         }
-      }
+      ]
+    },
+    output: {
+      filename: '0.0.1.js'
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 9000
+    },
+    plugins: [
+      new webpack.DefinePlugin(envKeys)
     ]
-  },
-  output: {
-    filename: '0.0.1.js'
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000
   }
 };
